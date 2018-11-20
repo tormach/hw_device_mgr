779ce4788a2cfe2f21a7fedc6ecf9d1a79547160
bdebruijn <bdebruijn@luminize.nl>
2018-11-20 17:09:47 +0100

    hal_402_drives_mgr: add status service pins
    these changes add functions being calledby the
    cb_robot_state_service callbackstates are
    traversed so until the targetstate is reached.
    
    when in "sim" mode, the HAL input pins are
    set according to the desired stateof the drive.

--- scripts/hal_402_drive.py
+++ scripts/hal_402_drive.py
@@ -1,5 +1,6 @@
 import rospy
 import hal
+import time
 # import messages from the ROS node
 from hal_402_device_mgr.msg import msg_error, msg_status
 
@@ -20,23 +21,24 @@ class pin_402(object):
     def create_halpin(self):
         self.halpin = self.parent_comp.newpin(self.name, self.type, self.dir)
 
-    def set_local_value(self):
-        # put HAL pin value in lacal_pin_value
+    def set_local_value(self, value):
+        self.local_pin_value = value
+
+    def set_hal_value(self):
+        self.halpin.set(self.local_pin_value)
+
+    def get_hal_value(self):
         self.local_pin_value = self.halpin.get()
 
+    def sync_hal(self):
+        if (self.dir == hal.HAL_OUT):
+            self.set_hal_value()
+        else:
+            self.get_hal_value()
 
-class drive_402(object):
 
-    def __init__(self, drive_name, parent):
-        # hal_402_drives_mgr
-        self.parent = parent
-        # bitmask and value
-        self.drive_name = drive_name
-        self.prev_state = 'unknown'
-        self.curr_state = 'unknown'
-        self.prev_status_word = 0
-        self.curr_status_word = 0
-        self.states_402 = {
+class state_machine_402(object):
+        states_402 = {
             'NOT READY TO SWITCH ON':   [0x4F, 0x00],
             'SWITCH ON DISABLED':       [0x4F, 0x40],
             'READY TO SWITCH ON':       [0x6F, 0x21],
@@ -46,6 +48,75 @@ class drive_402(object):
             'FAULT REACTION ACTIVE':    [0x4F, 0x0F],
             'QUICK STOP ACTIVE':        [0x6F, 0x07]
         }
+        # this dict shows what the next state should be when from a current
+        # state. Follow these steps until the state 'OPERATION ENABLED' has
+        # been reached. The vaulue of the dict entry contains a list of the
+        # next state, as well as the transition key. NA means "Not
+        # Available" because transition happens automatically by the device.
+        # These are kept around for keeping the complete picture.
+        path_to_operation_enabled = {
+            'NOT_READY_TO_SWITCH_ON':   ['SWITCH ON DISABLED', 'NA'],
+            'FAULT REACTION ACTIVE':    ['FAULT', 'NA'],
+            'FAULT':                    ['SWITCH ON DISABLED', 'TRANSITION_15'],
+            'QUICK STOP ACTIVE':        ['SWITCH ON DISABLED', 'NA'],
+            'SWITCH ON DISABLED':       ['READY TO SWITCH ON', 'TRANSITION_2'],
+            'READY TO SWITCH ON':       ['SWITCHED ON', 'TRANSITION_3'],
+            'SWITCHED ON':              ['OPERATION ENABLED', 'TRANSITION_4']
+        }
+        # these transitions take longer from OPERATION ENABLED -> SWITCH ON DISABLED
+        # 'OPERATION ENABLED':        ['SWITCHED ON', 'TRANSITION_5'],
+        # 'SWITCHED ON':              ['READY TO SWITCH ON', 'TRANSITION_6'],
+        # 'READY TO SWITCH ON':       ['SWITCH ON DISABLED', 'TRANSITION_7']
+        path_to_switch_on_disabled = {
+            'FAULT REACTION ACTIVE':    ['FAULT', 'NA'],
+            'FAULT':                    ['SWITCH ON DISABLED', 'TRANSITION_15'],
+            'NOT_READY_TO_SWITCH_ON':   ['SWITCH ON DISABLED', 'NA'],
+            'QUICK STOP ACTIVE':        ['SWITCH ON DISABLED', 'NA'],
+            'OPERATION ENABLED':        ['SWITCH ON DISABLED', 'TRANSITION_9']
+        }
+
+        # the transition dict contains a list of tuples with bits and value
+        # to can be set and reset.
+        transitions = {
+            'TRANSITION_2':             [('switch_on', 1)],
+            'TRANSITION_3':             [('enable_voltage', 1),
+                                         ('quick_stop', 1)],
+            'TRANSITION_4':             [('enable_operation', 1)],
+            'TRANSITION_5':             [('enable_operation', 0)],
+            'TRANSITION_6':             [('quick_stop', 0),
+                                         ('enable_voltage', 0)],
+            'TRANSITION_7':             [('switch_on', 0)],
+            'TRANSITION_8':             [('enable_operation', 0),
+                                         ('quick_stop', 0),
+                                         ('enable_voltage', 0)],
+            'TRANSITION_9':             [('enable_operation', 0),
+                                         ('quick_stop', 0),
+                                         ('enable_voltage', 0),
+                                         ('switch_on', 0)],
+            'TRANSITION_10':            [('quick_stop', 0),
+                                         ('enable_voltage', 0),
+                                         ('switch_on', 0)],
+            'TRANSITION_15':            [('enable_operation', 0),
+                                         ('quick_stop', 0),
+                                         ('enable_voltage', 0),
+                                         ('switch_on', 0),
+                                         ('fault_reset', 0)]
+        }
+
+
+class drive_402(object):
+
+    def __init__(self, drive_name, parent):
+        # hal_402_drives_mgr
+        self.sim = False
+        self.parent = parent
+        self.drive_name = drive_name
+        # bitmask and value
+        self.prev_state = 'unknown'
+        self.curr_state = 'unknown'
+        self.prev_status_word = 0
+        self.curr_status_word = 0
+        self.active_transition_table = None
         self.pins_402 = {
             # bits 0-3 and 7 and 8 of the controlword, bit 4-6 and
             # 9 - 15 intentionally not implemented yest
@@ -126,6 +197,45 @@ class drive_402(object):
         }
         self.create_pins()
 
+    def sim_set_input_status_pins(self, status):
+        # bitmask = state_machine_402.states_402[status][0]
+        # set pins according entire control word value
+        # effectively also resetting pins that are not in bitmask
+        statusword = state_machine_402.states_402[status][1]
+        for key, pin in self.pins_402.items():
+            if pin.dir == hal.HAL_IN:
+                # get value corresponding with bit index
+                # returns 1,2,4,8 depending on bit val == 1
+                # comparing a non-zero return gives a true or false (0 or 1)
+                bit_val = ((statusword & (1 << pin.bit_pos)) != 0)
+                # for simulation purpose when no drive available
+                pin.set_local_value(bit_val)
+                pin.set_hal_value()
+
+    def set_transition_table(self, transition_table):
+        self.active_transition_table = transition_table
+
+    def next_transition(self):
+        # when called:
+        # - look up the self.active_transition_table[self.curr_state]
+        # - get transition list[1]
+        transition = self.active_transition_table[self.curr_state][1]
+        # - in sim mode, get the the next state to mimic input pin changes
+        if (self.sim is True):
+            next_state = self.active_transition_table[self.curr_state][0]
+        # - look up transition in transition_table
+        # - get list with tuples containing pin and value to be set
+        change_pins_list = state_machine_402.transitions[transition]
+        # - for each tuple from list, set pin and value
+        for pin_change in change_pins_list:
+            self.change_halpin(pin_change)
+            # for simulation purpose, set input pins manually according to
+            # the next state as if the drive is attached
+            if (self.sim is True):
+                self.sim_set_input_status_pins(next_state)
+            # give HAL time to make at least 1 cycle
+            time.sleep(0.002)
+
     def create_pins(self):
         for key, pin in self.pins_402.items():
             pin.set_parent_comp(self.parent.halcomp)
@@ -162,13 +272,12 @@ class drive_402(object):
         # get all the status pins, and save their value locally
         for key, pin in self.pins_402.items():
             if pin.dir == hal.HAL_IN:
-                pin.set_local_value()
+                pin.sync_hal()
 
     def calculate_status_word(self):
-        # traverse dict and for the local values do some
-        # bitwise operation so that these input pins build
-        # up the current status word. The status word will
-        # be used for determining the 402 profile drive state
+        # traverse dict and for the local values do some bitwise operation so
+        # that these input pins build up the current status word. The status
+        # word will be used for determining the 402 profile drive state.
         self.prev_status_word = self.curr_status_word
         self.curr_status_word = 0
         for key, pin in self.pins_402.items():
@@ -178,17 +287,20 @@ class drive_402(object):
 
     def calculate_state(self):
         self.prev_state = self.curr_state
-        for key, state in self.states_402.items():
+        for key, state in state_machine_402.states_402.items():
             # check if the value after applying the bitmask (value[0])
             # corresponds with the value[1] to determine the current status
             bitmaskvalue = self.curr_status_word & state[0]
-            print('--- %s' % key)
-            print('{:#010b} '.format(state[0]))
-            print('{:#010b} '.format(state[1]))
-            print('{:#010b} curr_status_word'.format(self.curr_status_word))
-            print('{:#010b} bitmaskvalue'.format(bitmaskvalue))
+            # print('--- %s' % key)
+            # print('{:#010b} bitmask'.format(state[0]))
+            # print('{:#010b} value'.format(state[1]))
+            # print('{:#010b} curr_status_word'.format(self.curr_status_word))
+            # print('{:#010b} bitmaskvalue'.format(bitmaskvalue))
+            # print('bitmaskvalue == state[1] : %s' % (bitmaskvalue == state[1]))
             if (bitmaskvalue == state[1]):
                 self.curr_state = key
+                # exit when we've reached correct state
+                break
             else:
                 self.curr_state = 'unknown'
 
@@ -196,14 +308,10 @@ class drive_402(object):
         if self.drive_state_changed():
             self.topics['status'].publish(self.drive_name, self.curr_state)
 
-    def set_halpins():
-        pass
-
-    def enable_drive():
-        pass
-
-    def disable_drive():
-        pass
+    def change_halpin(self, pin_change):
+        pin = self.pins_402[pin_change[0]]  # pick the pin from the list
+        pin.set_local_value(pin_change[1])  # and set local value accordingly
+        pin.sync_hal()                      # then sync the pin local value with HAL
 
     def drive_state_changed(self):
         # only publish drive status if the status has changed
--- scripts/hal_402_mgr.py
+++ scripts/hal_402_mgr.py
@@ -1,8 +1,10 @@
 import rospy
 import hal
+import time
 # import service messages from the ROS node
 from hal_402_device_mgr.srv import srv_robot_state
 from hal_402_drive import drive_402 as drive_402
+from hal_402_drive import state_machine_402 as state_402
 
 
 class hal_402_mgr(object):
@@ -12,6 +14,18 @@ class hal_402_mgr(object):
         self.drives = dict()
         self.prev_robot_state = 'unknown'
         self.curr_robot_state = 'unknown'
+        # very simple transitions for now
+        # unknown -> stopped
+        # error -> stopped
+        # stopped -> error
+        # stopped -> started
+        # started -> error
+        self.states = {
+            'unknown':  [state_402.path_to_switch_on_disabled, 'SWITCH ON DISABLED'],
+            'started':  [state_402.path_to_operation_enabled, 'OPERATION ENABLED'],
+            'stopped':  [state_402.path_to_switch_on_disabled, 'SWITCH ON DISABLED'],
+            'error':    [state_402.path_to_switch_on_disabled, 'SWITCH ON DISABLED']
+        }
 
         # create ROS node
         rospy.init_node(self.compname)
@@ -24,6 +38,10 @@ class hal_402_mgr(object):
         # create drives which create pins
         self.create_drives()
         self.halcomp.ready()
+
+        # for testing and debug purpose
+        self.sim_set_drivestates('FAULT')
+        self.sim_set_drive_sim(True)
 
         self.create_service()
         self.create_publisher()
@@ -42,14 +60,70 @@ class hal_402_mgr(object):
         # create publishers for topics and send out a test message
         for key, drive in self.drives.items():
             drive.create_topics()
-            drive.test_publisher()
+            if (drive.sim is True):
+                drive.test_publisher()
+
+    def all_equal_status(self, status):
+        # check if all the drives have the same status
+        for key, drive in self.drives.items():
+            if not (drive.curr_state == status):
+                return False
+        return True
+
+    def sim_set_drive_sim(self, status):
+        # set simulation attribute to mimic hanging statusword
+        for key, drive in self.drives.items():
+            drive.sim = True
+
+    def sim_set_drivestates(self, status):
+        for key, drive in self.drives.items():
+            drive.sim_set_input_status_pins(status)
+        # give HAL at least 1 cycle to process
+        time.sleep(0.002)
+        self.inspect_hal_pins()
 
     def cb_robot_state_service(self, req):
-        # the value of the requested state is in req.req_state
+        # The service callback
+        # the value of the requested state is in req.req_state (string)
+        # the return value is the service response (string)
+        # check the requested state for validity
+        if req.req_state not in self.states:
+            rospy.loginfo("%s: request for state failed, %s not a valid state" %
+                          (self.compname,
+                           req.req_state))
+            return "request for state %s failed, state not known" % req.req_state
+        # pick a transition table for the requested state
+        tr_table = self.states[req.req_state][0]
+        all_target_states = self.states[req.req_state][1]
+        # set "active" transition table for the drive
+        for key, drive in self.drives.items():
+            drive.set_transition_table(tr_table)
+
+        # the drive returns success if all the drives are :
+        # OPERATION ENABLED or SWITCH ON DISABLED or max_attempts
+        i = 0
+        max_attempts = len(state_402.states_402)
+        while ((not self.all_equal_status(all_target_states))
+               or not (i in range(0, max_attempts))):
+            for key, drive in self.drives.items():
+                # traverse states for all drives (parallel)
+                # ignore drives which state already is at the target state
+                if not (drive.curr_state == all_target_states):
+                    drive.next_transition()
+            self.inspect_hal_pins()
+            self.publish_states()
+            i += 1
+
+        # when states are successfully reached, update overall state
+        # when all statuses have been reached, success
+        # otherwise num_states has overflowed
+        if self.all_equal_status(all_target_states):
+            self.curr_robot_state = req.req_state
+        else:
+            self.curr_robot_state = 'error'
         return self.curr_robot_state
 
     def cb_test_service_cb(self, req):
-        print(req.req_state)  # show abcd in the terminsal
         rospy.loginfo("%s: cb_test_service" % self.compname)
         # the response that's returned is the return value of the callback
         return "test service return string"
@@ -74,11 +148,11 @@ class hal_402_mgr(object):
             drive.read_halpins()
             drive.calculate_status_word()
             drive.calculate_state()
-            print('{}: {} status_word: {:#010b} status: {}'.format(
-                                                   self.compname,
-                                                   drive.drive_name,
-                                                   drive.curr_status_word,
-                                                   drive.curr_state))
+            # print('{}: {} status_word: {:#010b} status: {}'.format(
+            #                                        self.compname,
+            #                                        drive.drive_name,
+            #                                        drive.curr_status_word,
+            #                                        drive.curr_state))
 
     def publish_states(self):
         for key, drive in self.drives.items():
