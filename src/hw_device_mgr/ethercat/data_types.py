bea14c0e90f57ea5d3053ee345fb5213366e07d9
John Morris <john@zultron.com>
2021-09-22 17:26:16 -0500

    Add EtherCAT ETG.2000 data types
    EtherCAT CoE device object dictionaries are very similar CiA 301's.  The
    ETG.2000 standared defines EtherCAT CoE data types, compatible with
    CiA 301, and with standardized names, at least.

---
+++ src/hw_device_mgr/ethercat/data_types.py
@@ -0,0 +1,31 @@
+from ..data_types import DataType
+
+
+class EtherCATDataType(DataType):
+    """Data types defined by ETG.2000"""
+
+    subtype_prefix = "EtherCAT"
+    subtype_data = dict(
+        bit=dict(
+            name="BOOL",
+            name_re=r"BOOL|BIT",
+        ),
+        int8=dict(name="SINT"),
+        int16=dict(name="INT"),
+        int32=dict(name="DINT"),
+        int64=dict(name="LINT"),
+        uint8=dict(name="USINT"),
+        uint16=dict(name="UINT"),
+        uint32=dict(name="UDINT"),
+        uint64=dict(name="ULINT"),
+        float=dict(name="REAL"),  # Never seen in the wild
+        double=dict(name="LREAL"),  # Never seen in the wild
+        str=dict(
+            # Sequence of octets
+            name="STRING",
+            name_re=r"STRING(\([0-9]+\))?",
+        ),
+    )
+
+
+# ETG.2000 defines other types
