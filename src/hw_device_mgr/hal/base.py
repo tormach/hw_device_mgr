e1d4f8ba5ebae4cb10ab4d9857d378980b3ffb8d
John Morris <john@zultron.com>
2021-09-26 18:19:30 -0500

    hal initial commit

---
+++ src/hw_device_mgr/hal/base.py
@@ -0,0 +1,47 @@
+import hal
+
+
+class HALMixin:
+    """A Machinekit HAL-related mixin class
+
+    Provides `hal` enums as class attrs, e.g. `HAL_IN`, `HAL_OUT`,
+    `HAL_BIT`, `HAL_U32`, and methods for smoother HAL integration.
+    """
+
+    # - Set class attrs: HAL_IN, HAL_OUT, HAL_BIT, HAL_U32, etc. `hal` enums
+    # - Set up int to str map
+    _hal_enum_to_str_map = dict()
+    for name in dir(hal):
+        val = getattr(hal, name)
+        if not name.startswith("HAL_") or not isinstance(val, int):
+            continue
+        locals()[name] = val
+        _hal_enum_to_str_map[val] = name
+
+    @classmethod
+    def hal_enum(cls, x):
+        """Translates a `string` or `int` to a HAL enum.
+
+        E.g. `in`, `IN` or `16` -> `hal.HAL_IN`.
+        """
+        return getattr(hal, f"HAL_{x.upper()}") if isinstance(x, str) else x
+
+    @classmethod
+    def hal_enum_str(cls, x):
+        """Translates a HAL enum to a `string`.
+
+        E.g. `3`, `hal.HAL_S32` -> `HAL_S32`
+        """
+        return cls._hal_enum_to_str_map[x]
+
+    @staticmethod
+    def pname_to_attr(pname):
+        """Munges a HAL pin name to be suitable as a Python name.
+
+        E.g. `control-word` -> `control_word`
+        """
+        return pname.replace("-", "_")
+
+    @property
+    def hal(self):
+        return hal
---
+++ src/hw_device_mgr/hal/tests/test_device.py
@@ -0,0 +1,92 @@
+import pytest
+from .base_test_class import BaseHALTestClass
+from ...cia_301.tests.test_device import TestCiA301Device as _TestCiA301Device
+from pprint import pformat
+
+
+class TestHALDevice(BaseHALTestClass, _TestCiA301Device):
+    halcomp_name = "hal_device"
+
+    expected_mro = [
+        "BogusHALDevice",
+        "HALPinDevice",
+        "BogusCiA301Device",
+        "CiA301Device",
+        "Device",
+        "ABC",
+        "HALMixin",
+        "object",
+    ]
+
+    @pytest.fixture
+    def obj(self, device_data, mock_halcomp):
+        self.obj = self.device_model_cls(
+            address=device_data["address"], sim=self.sim
+        )
+        self.obj.init(mock_halcomp)
+        yield self.obj
+
+    def test_pin_interfaces(self, device_cls):
+        for intf, data in device_cls.pin_interfaces.items():
+            print(f"intf:  {intf};  data:  {data}")
+            assert intf in device_cls.interface_names
+
+    def test_init(self, obj):
+        super().test_init(obj)
+
+        assert obj.comp is self.mock_halcomp
+        print(f"pins:\n{pformat(obj.pins)}")
+        pnames = set()
+        for intf, data in obj.pin_interfaces.items():
+            print(f"interface:  {intf};  data:  {data}")
+            for name in obj.interface(intf).get():
+                pname = obj.pin_name(intf, name)
+                assert pname in obj.pins
+                pnames.add(pname)
+        assert pnames == set(obj.pins.keys())
+
+    #########################################
+    # Test read()/update()/write() integration
+    #
+
+    def override_interface_param(self, interface, key, val):
+        intf = self.obj.interface(interface)
+        intf.update(**{key: val})
+        dt_names = self.obj.merge_dict_attrs(f"{interface}_data_types")
+        dt = dt_names.get(key, None)
+        if dt is not None:
+            val = self.obj.data_type_class.by_shared_name(dt)(val)
+        pname = self.obj.pin_name(interface, key)
+        self.set_pin(pname, val)
+
+    def copy_sim_feedback(self, obj=None):
+        if obj is None:
+            obj = self.obj
+            print(
+                "\n*** Copying HAL pin values from sim_feedback to feedback_in"
+            )
+        for name in obj.sim_feedback.get():
+            sfbpname = obj.pin_name("sim_feedback", name)
+            fbpname = obj.pin_name("feedback_in", name)
+            sfb_val = self.get_pin(sfbpname)
+            self.set_pin(fbpname, sfb_val)
+            fb_val = self.get_pin(fbpname)
+            assert sfb_val == fb_val
+
+    def pre_read_actions(self):
+        self.copy_sim_feedback()
+
+    def check_halpin_values(self, obj=None):
+        if obj is None:
+            obj = self.obj
+            print("\n*** Checking halpin values were written as expected")
+        for iface in ("sim_feedback", "command_out"):
+            print(f"  {iface}:")
+            for name, iface_val in obj.interface(iface).get().items():
+                pname = obj.pin_name(iface, name)
+                pin_val = self.get_pin(pname)
+                print(f"    {pname}={pin_val}, {name}={iface_val}")
+                assert pin_val == iface_val
+
+    def post_write_actions(self):
+        self.check_halpin_values()
