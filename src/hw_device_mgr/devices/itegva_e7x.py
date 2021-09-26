e0011f404e201d054c23facf43e1941faa0ddc54
John Morris <john@zultron.com>
2021-09-26 18:19:48 -0500

    devices initial commit

---
+++ src/hw_device_mgr/devices/itegva_e7x.py
@@ -0,0 +1,70 @@
+from ..ethercat.device import EtherCATDevice
+
+
+class ITegvaE7xDevice(EtherCATDevice):
+    """Base class for iTegva E7x series IO modules"""
+
+    vendor_id = 0x00000A09
+    xml_description_fname = "iTegva_E7x_Series.xml"
+
+
+# "Access_Bit":  IOs individually accessible via bit PDOs
+
+
+class ITegvaE7820001BitDevice(ITegvaE7xDevice):
+    """E7.820.001 16-ch Dig.In/16-ch Relay Out(Access_Bit)"""
+
+    name = "itegva_E7.820.001_bit"
+    product_code = 0x00000100
+
+
+class ITegvaE7820002BitDevice(ITegvaE7xDevice):
+    """E7.820.002 32-ch Dig.Input(Access_Bit)"""
+
+    name = "itegva_E7.820.002_bit"
+    product_code = 0x00000300
+
+
+class ITegvaE7820003BitDevice(ITegvaE7xDevice):
+    """E7.820.003 16-ch Dig.In/16-ch Mosfet Out(Access_Bit)"""
+
+    name = "itegva_E7.820.003_bit"
+    product_code = 0x00000200  # [sic]
+
+
+class ITegvaE7820004BitDevice(ITegvaE7xDevice):
+    """E7.820.004 32-ch Dig.Output(Access_Bit)"""
+
+    name = "itegva_E7.820.004_bit"
+    product_code = 0x00000400
+
+
+# "Access_Byte":  IOs accessible via uint8 PDOs
+
+
+class ITegvaE7820001ByteDevice(ITegvaE7xDevice):
+    """E7.820.001 16-ch Dig.In/16-ch Relay Out(Access_Byte)"""
+
+    name = "itegva_E7.820.001_byte"
+    product_code = 0x00000101
+
+
+class ITegvaE7820002ByteDevice(ITegvaE7xDevice):
+    """E7.820.002 32-ch Dig.Input(Access_Byte)"""
+
+    name = "itegva_E7.820.002_byte"
+    product_code = 0x00000301
+
+
+class ITegvaE7820003ByteDevice(ITegvaE7xDevice):
+    """E7.820.003 16-ch Dig.In/16-ch Mosfet Out(Access_Byte)"""
+
+    name = "itegva_E7.820.003_byte"
+    product_code = 0x00000201  # [sic]
+
+
+class ITegvaE7820004ByteDevice(ITegvaE7xDevice):
+    """E7.820.004 32-ch Dig.Output(Access_Byte)"""
+
+    name = "itegva_E7.820.004_byte"
+    product_code = 0x00000401
