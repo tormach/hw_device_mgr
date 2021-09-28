e03097d33fa910c66adf623d72ebfe7988350017
John Morris <john@zultron.com>
2021-09-28 08:58:18 -0500

    Add `package_data` from `devices` module to `setup.py`

--- setup.py
+++ setup.py
@@ -3,8 +3,28 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
+dev_data_dir = "src/hw_device_mgr/devices"
 d = generate_distutils_setup(
-    packages=["hw_device_mgr"], package_dir={"": "src"}
+    packages=["hw_device_mgr"],
+    package_dir={"": "src"},
+    package_data=[
+        (
+            "drive_err",
+            [
+                f"{dev_data_dir}/IS620N.yaml",
+                f"{dev_data_dir}/SV660N.yaml",
+                # (No errors for iTegva_E7x)
+            ],
+        ),
+        (
+            "drive_xml",
+            [
+                f"{dev_data_dir}/IS620N_v2.6.7.xml",
+                f"{dev_data_dir}/SV660_EOE_1Axis_V9.12.xml",
+                f"{dev_data_dir}/iTegva_E7x_Series.xml",
+            ],
+        ),
+    ],
 )
 
 setup(**d)
