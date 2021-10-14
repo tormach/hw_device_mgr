4eaa974deaeb811dbc8b86822b2392afea29f44e
John Morris <john@zultron.com>
2021-10-14 10:47:51 -0500

    cia_301 data types:  Don't re-run parent class tests
    Hide parent test class by prefixing with `_` so that `pytest` doesn't
    re-run the tests for that class.

--- src/hw_device_mgr/cia_301/tests/test_data_types.py
+++ src/hw_device_mgr/cia_301/tests/test_data_types.py
@@ -1,8 +1,8 @@
 from ..data_types import CiA301DataType
-from ...tests.test_data_types import TestDataType
+from ...tests.test_data_types import TestDataType as _TestDataType
 
 
-class TestCiA301DataType(TestDataType):
+class TestCiA301DataType(_TestDataType):
     tc_base = CiA301DataType
     defined_shared_types = {
         "int8",
