26eee488d34761f39733c6eda5807136a2da6031
John Morris <john@zultron.com>
2021-10-28 13:53:58 -0500

    Fix packaging problems causing CI build failures

--- setup.py
+++ setup.py
@@ -4,27 +4,46 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 dev_data_dir = "src/hw_device_mgr/devices"
+# Packages like hw_device_mgr.{pkg}.tests.bogus_devices
+pkgs_bd = [
+    "cia_301",
+    "cia_402",
+    "errors",
+    "ethercat",
+    "hal",
+    "lcec",
+    "mgr",
+    "mgr_hal",
+    "mgr_ros",
+]
+# Packages like hw_device_mgr.{pkg}.tests
+pkgs_t = ["devices", "mgr_ros_hal"] + pkgs_bd
+# Generate lists
+packages = (
+    [
+        "hw_device_mgr",
+        "hw_device_mgr.tests",
+        "hw_device_mgr.tests.bogus_devices",
+    ]
+    + [f"hw_device_mgr.{p}" for p in pkgs_t]
+    + [f"hw_device_mgr.{p}.tests" for p in pkgs_t]
+    + [f"hw_device_mgr.{p}.tests.bogus_devices" for p in pkgs_bd]
+)
+
 d = generate_distutils_setup(
-    packages=["hw_device_mgr"],
+    packages=packages,
     package_dir={"": "src"},
-    package_data=[
-        (
-            "drive_err",
-            [
-                f"{dev_data_dir}/IS620N.yaml",
-                f"{dev_data_dir}/SV660N.yaml",
-                # (No errors for iTegva_E7x)
-            ],
-        ),
-        (
-            "drive_xml",
-            [
-                f"{dev_data_dir}/IS620N_v2.6.7.xml",
-                f"{dev_data_dir}/SV660_EOE_1Axis_V9.12.xml",
-                f"{dev_data_dir}/iTegva_E7x_Series.xml",
-            ],
-        ),
-    ],
+    package_data={
+        "": [  # Within any package, install:
+            # ESI files
+            "device_xml/*.xml",
+            # Error descriptions
+            "device_err/*.yaml",
+            # Test configs
+            "tests/*.yaml",
+            "bogus_devices/*.yaml",
+        ],
+    },
 )
 
 setup(**d)
