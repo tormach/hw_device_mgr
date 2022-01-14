f9eac02fa318c2f0c73da989adeceb9078cae224
John Morris <john@zultron.com>
2022-01-14 12:48:33 -0600

    Update HAL for sim device changes

--- hw_device_mgr/hal/tests/bogus_devices/device.py
+++ hw_device_mgr/hal/tests/bogus_devices/device.py
@@ -1,26 +1,27 @@
-from ....cia_301.tests.bogus_devices.device import (
-    BogusCiA301Device,
-    BogusCiA301Servo,
-    BogusCiA301Servo2,
-    BogusCiA301IO,
-)
 from ...device import HALPinDevice
+from ....cia_301.tests.bogus_devices.device import (
+    BogusCiA301DeviceCategory,
+    BogusV1CiA301ServoCategory,
+    BogusV2CiA301ServoCategory,
+    BogusV1CiA301IOCategory,
+)
 
 
-class BogusHALDevice(HALPinDevice, BogusCiA301Device):
+class BogusHALDevice(HALPinDevice, BogusCiA301DeviceCategory):
     category = "bogus_hal_device"
+    vendor_id = 0xB090C0
 
 
-class BogusHALServo(BogusHALDevice, BogusCiA301Servo):
-    name = "bogo_hal_servo"
+class BogusV1HALServo(BogusHALDevice, BogusV1CiA301ServoCategory):
+    name = "bogo_v1_hal_servo"
     product_code = 0xB0905050
 
 
-class BogusHALServo2(BogusHALDevice, BogusCiA301Servo2):
-    name = "bogo_hal_servo_2"
+class BogusV2HALServo(BogusHALDevice, BogusV2CiA301ServoCategory):
+    name = "bogo_v2_hal_servo"
     product_code = 0xB0905051
 
 
-class BogusHALIO(BogusHALDevice, BogusCiA301IO):
-    name = "bogo_hal_io"
+class BogusV1HALIO(BogusHALDevice, BogusV1CiA301IOCategory):
+    name = "bogo_v1_hal_io"
     product_code = 0xB0901050
