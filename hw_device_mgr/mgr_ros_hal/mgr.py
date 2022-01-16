0232249a29e8cff5882fb89a6e0c7f97193ef6f2
John Morris <john@zultron.com>
2022-01-16 17:03:22 -0600

    Update mgr_ros_hal for sim device changes

--- hw_device_mgr/mgr_ros_hal/mgr.py
+++ hw_device_mgr/mgr_ros_hal/mgr.py
@@ -1,10 +1,29 @@
 from ..mgr_hal.mgr import HALHWDeviceMgr
 from ..mgr_ros.mgr import ROSHWDeviceMgr
-from .devices import ManagedDevices
+from ..mgr.mgr import SimHWDeviceMgr
+from .devices import (
+    ManagedDevices,
+    SimManagedLCECDevices,
+    SimManagedEtherCATDevices,
+)
 
 
 class ROSHALHWDeviceMgr(ROSHWDeviceMgr, HALHWDeviceMgr):
-
+    name = "ros_hal_hw_device_mgr"
     data_type_class = ManagedDevices.data_type_class
     device_base_class = ManagedDevices
     device_classes = ManagedDevices.get_model()
+
+
+class SimROSLCECHWDeviceMgr(ROSHALHWDeviceMgr, SimHWDeviceMgr):
+
+    name = "sim_ros_lcec_hw_device_mgr"
+    device_base_class = SimManagedLCECDevices
+    device_classes = SimManagedLCECDevices.get_model()
+
+
+class SimROSEtherCATHWDeviceMgr(ROSHALHWDeviceMgr, SimHWDeviceMgr):
+
+    name = "sim_ros_ethercat_hw_device_mgr"
+    device_base_class = SimManagedEtherCATDevices
+    device_classes = SimManagedEtherCATDevices.get_model()
--- hw_device_mgr/mgr_ros_hal/tests/test_mgr.py
+++ hw_device_mgr/mgr_ros_hal/tests/test_mgr.py
@@ -1,20 +1,24 @@
 from .base_test_class import BaseROSHALMgrTestClass
-from ...mgr_ros.tests.test_mgr import TestROSDeviceMgr as _TestROSDeviceMgr
+from ...mgr_ros.tests.test_mgr import TestROSHWDeviceMgr as _TestROSHWDeviceMgr
 from ...mgr_hal.tests.test_mgr import TestHALHWDeviceMgr as _TestHALHWDeviceMgr
 
 
-class TestROSDeviceMgr(
-    BaseROSHALMgrTestClass, _TestROSDeviceMgr, _TestHALHWDeviceMgr
+class TestROSHWDeviceMgr(
+    BaseROSHALMgrTestClass, _TestROSHWDeviceMgr, _TestHALHWDeviceMgr
 ):
 
     expected_mro = [
+        "BogusHALROSHWDeviceMgr",
+        "SimROSEtherCATHWDeviceMgr",
         "ROSHALHWDeviceMgr",
         "ROSHWDeviceMgr",
         "HALHWDeviceMgr",
+        "SimHWDeviceMgr",
         "HWDeviceMgr",
         "FysomGlobalMixin",
         "HALCompDevice",
         "HALPinDevice",
+        "SimDevice",
         "Device",
         "ABC",
         "HALMixin",
