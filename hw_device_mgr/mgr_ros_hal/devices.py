8525d4a2ff4c83b90e0f8014686f94016bc5fc44
John Morris <john@zultron.com>
2022-01-16 17:03:22 -0600

    Update mgr_ros_hal for sim device changes

--- hw_device_mgr/mgr_ros_hal/devices.py
+++ hw_device_mgr/mgr_ros_hal/devices.py
@@ -1,13 +1,16 @@
 # Real devices under LCEC
-from ..lcec.device import LCECDevice
-from ..cia_402.device import CiA402Device
+from ..hal.device import HALPinDevice, HALDataType
+from ..lcec.device import LCECDevice, LCECSimDevice
+from ..ethercat.device import EtherCATSimDevice
+from ..cia_402.device import CiA402Device, CiA402SimDevice
 from ..devices.elmo_gold import ElmoGold420, ElmoGold520
 from ..devices.inovance_is620n import InovanceIS620N
 from ..devices.inovance_sv660 import InovanceSV660
+from ..devices.bogus import BogusServo
 
 
-class ManagedDevices(LCECDevice, CiA402Device):
-    category = "managed_devices"
+class ManagedDevices(LCECDevice, CiA402Device, HALPinDevice):
+    category = "managed_lcec_devices"
     allow_rereg = True  # Subclasses reuse model_id
 
 
@@ -24,4 +27,57 @@ class InovanceIS620NLCEC(InovanceIS620N, ManagedDevices):
 
 
 class InovanceSV660LCEC(InovanceSV660, ManagedDevices):
+    pass
+
+
+class SimManagedLCECDevices(LCECSimDevice, CiA402SimDevice, HALPinDevice):
+    category = "sim_lcec_managed_devices"
+    allow_rereg = True  # Subclasses reuse model_id
+
+
+class BogusServoSimLCEC(BogusServo, SimManagedLCECDevices):
+    pass
+
+
+class ElmoGold420SimLCEC(ElmoGold420, SimManagedLCECDevices):
+    pass
+
+
+class ElmoGold520SimLCEC(ElmoGold520, SimManagedLCECDevices):
+    pass
+
+
+class InovanceIS620NSimLCEC(InovanceIS620N, SimManagedLCECDevices):
+    pass
+
+
+class InovanceSV660SimLCEC(InovanceSV660, SimManagedLCECDevices):
+    pass
+
+
+class SimManagedEtherCATDevices(
+    EtherCATSimDevice, CiA402SimDevice, HALPinDevice
+):
+    category = "sim_ethercat_managed_devices"
+    allow_rereg = True  # Subclasses reuse model_id
+    data_type_class = HALDataType
+
+
+class BogusServoSimEtherCAT(BogusServo, SimManagedEtherCATDevices):
+    pass
+
+
+class ElmoGold420SimEtherCAT(ElmoGold420, SimManagedEtherCATDevices):
+    pass
+
+
+class ElmoGold520SimEtherCAT(ElmoGold520, SimManagedEtherCATDevices):
+    pass
+
+
+class InovanceIS620NSimEtherCAT(InovanceIS620N, SimManagedEtherCATDevices):
+    pass
+
+
+class InovanceSV660SimEtherCAT(InovanceSV660, SimManagedEtherCATDevices):
     pass
---
+++ hw_device_mgr/mgr_ros_hal/tests/bogus_devices/mgr.py
@@ -0,0 +1,20 @@
+from ...mgr import SimROSHALHWDeviceMgr
+from ....devices.tests.test_devices_402 import (
+    DevicesForTest,
+    ElmoGold420ForTest,
+    ElmoGold520ForTest,
+    InovanceIS620NForTest,
+    InovanceSV660NForTest,
+)
+
+
+class BogusHALROSHWDeviceMgr(SimROSHALHWDeviceMgr):
+    name = "bogus_hal_ros_hw_device_mgr"
+    data_type_class = DevicesForTest.data_type_class
+    device_base_class = DevicesForTest
+    device_classes = (
+        ElmoGold420ForTest,
+        ElmoGold520ForTest,
+        InovanceIS620NForTest,
+        InovanceSV660NForTest,
+    )
