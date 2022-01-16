a8fe6a4a42985c4685a7bbd4de219aa5828c1722
John Morris <john@zultron.com>
2022-01-16 17:03:22 -0600

    Update mgr_ros_hal for sim device changes

--- hw_device_mgr/mgr_ros_hal/devices.py
+++ hw_device_mgr/mgr_ros_hal/devices.py
@@ -1,27 +1,80 @@
 # Real devices under LCEC
-from ..lcec.device import LCECDevice
-from ..cia_402.device import CiA402Device
+from ..hal.device import HALPinDevice, HALDataType
+from ..lcec.device import LCECDevice, LCECSimDevice
+from ..ethercat.device import EtherCATSimDevice
+from ..cia_402.device import CiA402Device, CiA402SimDevice
 from ..devices.elmo_gold import ElmoGold420, ElmoGold520
 from ..devices.inovance_is620n import InovanceIS620N
 from ..devices.inovance_sv660 import InovanceSV660
+from ..devices.bogus import BogusServo
 
 
-class ManagedDevices(LCECDevice, CiA402Device):
-    category = "managed_devices"
-    allow_rereg = True  # Subclasses reuse model_id
+class ManagedDevices(LCECDevice, CiA402Device, HALPinDevice):
+    category = "managed_lcec_devices"
 
 
 class ElmoGold420LCEC(ElmoGold420, ManagedDevices):
-    pass
+    product_code = 0x20030924
 
 
 class ElmoGold520LCEC(ElmoGold520, ManagedDevices):
-    pass
+    product_code = 0x20030925
 
 
 class InovanceIS620NLCEC(InovanceIS620N, ManagedDevices):
-    pass
+    product_code = 0x200C0108
 
 
 class InovanceSV660LCEC(InovanceSV660, ManagedDevices):
-    pass
+    product_code = 0x200C010D
+
+
+class SimManagedLCECDevices(LCECSimDevice, CiA402SimDevice, HALPinDevice):
+    category = "sim_lcec_managed_devices"
+
+
+class BogusServoSimLCEC(BogusServo, SimManagedLCECDevices):
+    product_code = 0xB09050F2
+
+
+class ElmoGold420SimLCEC(ElmoGold420, SimManagedLCECDevices):
+    product_code = 0x30030924
+
+
+class ElmoGold520SimLCEC(ElmoGold520, SimManagedLCECDevices):
+    product_code = 0x30030925
+
+
+class InovanceIS620NSimLCEC(InovanceIS620N, SimManagedLCECDevices):
+    product_code = 0x300C0108
+
+
+class InovanceSV660SimLCEC(InovanceSV660, SimManagedLCECDevices):
+    product_code = 0x300C010D
+
+
+class SimManagedEtherCATDevices(
+    EtherCATSimDevice, CiA402SimDevice, HALPinDevice
+):
+    category = "sim_ethercat_managed_devices"
+    data_type_class = HALDataType
+
+
+class BogusServoSimEtherCAT(BogusServo, SimManagedEtherCATDevices):
+    product_code = 0xB09050F3
+
+
+class ElmoGold420SimEtherCAT(ElmoGold420, SimManagedEtherCATDevices):
+    product_code = 0x40030924
+
+
+class ElmoGold520SimEtherCAT(ElmoGold520, SimManagedEtherCATDevices):
+    product_code = 0x40030925
+
+
+class InovanceIS620NSimEtherCAT(InovanceIS620N, SimManagedEtherCATDevices):
+    product_code = 0x400C0108
+
+
+class InovanceSV660SimEtherCAT(InovanceSV660, SimManagedEtherCATDevices):
+    product_code = 0x400C010D
--- hw_device_mgr/mgr_ros_hal/tests/base_test_class.py
+++ hw_device_mgr/mgr_ros_hal/tests/base_test_class.py
@@ -1,6 +1,7 @@
 from ...mgr_hal.tests.base_test_class import BaseLCEC402MgrTestClass
 from ...mgr_ros.tests.base_test_class import BaseROSMgrTestClass
-from ..mgr import ROSHALHWDeviceMgr
+from .bogus_devices.mgr import BogusHALROSHWDeviceMgr
+from ...devices.tests.test_devices_402 import Test402Devices as _Test402Devices
 import pytest
 
 
@@ -11,18 +12,19 @@ import pytest
 class BaseROSHALMgrTestClass(BaseLCEC402MgrTestClass, BaseROSMgrTestClass):
     """Base test class for `ROSHWDeviceMgr` class."""
 
-    # Managed device types
-    device_class = ROSHALHWDeviceMgr
-    data_type_class = ROSHALHWDeviceMgr.data_type_class
-    device_model_classes = ROSHALHWDeviceMgr.device_classes
+    data_type_class = BogusHALROSHWDeviceMgr.data_type_class
+    device_class = BogusHALROSHWDeviceMgr
+    device_base_class = BogusHALROSHWDeviceMgr.device_base_class
+    device_model_classes = BogusHALROSHWDeviceMgr.device_classes
 
-    # Test configuration
-    global_config_yaml = "devices/tests/config.yaml"
-    device_data_yaml = "devices/tests/device_config.yaml"
-    device_sdos_yaml = "devices/tests/sdos.yaml"
+    device_config_yaml = _Test402Devices.device_config_yaml
+    device_data_yaml = _Test402Devices.device_data_yaml
+    device_sdos_yaml = _Test402Devices.device_sdos_yaml
+
+    device_model_sdo_clone = None
 
     @pytest.fixture
-    def device_cls(self, config_cls, manager_ros_params, manager_lcec):
-        """Fixture for ROS + LCEC Device classes."""
-        self.device_class.clear_devices()
-        yield self.device_class
+    def extra_fixtures(
+        self, manager_ros_params, mock_hal, mock_ethercat_command
+    ):
+        pass
