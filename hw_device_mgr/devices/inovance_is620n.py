8e6c880a1ffbfbda6a4a2b1026b2e83e93c98b1c
John Morris <john@zultron.com>
2022-06-23 16:13:31 -0500

    devices:  Add error message translations to Inovance drives

--- hw_device_mgr/devices/inovance_is620n.py
+++ hw_device_mgr/devices/inovance_is620n.py
@@ -1,14 +1,17 @@
 from ..ethercat.device import EtherCATDevice
 from ..cia_402.device import CiA402Device
+from ..errors.device import ErrorDevice
 
 
-class InovanceIS620N(EtherCATDevice, CiA402Device):
+class InovanceIS620N(EtherCATDevice, CiA402Device, ErrorDevice):
     """Inovance IS620N servo drives."""
 
     vendor_id = 0x00100000
     product_code = 0x000C0108
     xml_description_package = "hw_device_mgr.devices.device_xml"
     xml_description_fname = "IS620N_v2.6.7.xml"
+    device_error_package = "hw_device_mgr.devices.device_err"
+    device_error_yaml = "inovance_is620n.yaml"
 
     def set_params_volatile(self, nv=False):
         # 0:  params not updated
--- hw_device_mgr/devices/inovance_sv660.py
+++ hw_device_mgr/devices/inovance_sv660.py
@@ -1,14 +1,17 @@
 from ..ethercat.device import EtherCATDevice
 from ..cia_402.device import CiA402Device
+from ..errors.device import ErrorDevice
 
 
-class InovanceSV660(EtherCATDevice, CiA402Device):
+class InovanceSV660(EtherCATDevice, CiA402Device, ErrorDevice):
     """Inovance SV660 servo drives."""
 
     vendor_id = 0x00100000
     product_code = 0x000C010D
     xml_description_package = "hw_device_mgr.devices.device_xml"
     xml_description_fname = "SV660_EOE_1Axis_V9.12.xml"
+    device_error_package = "hw_device_mgr.devices.device_err"
+    device_error_yaml = "inovance_sv660n.yaml"
 
     def set_params_volatile(self, nv=False):
         # 0:  params not updated
