77f42c4e2e6103026ce6de601c8a41c8d106d620
John Morris <john@zultron.com>
2022-06-29 15:56:46 -0500

    devices:  Move Inovance device param NV methods to config class

--- hw_device_mgr/devices/inovance_is620n.py
+++ hw_device_mgr/devices/inovance_is620n.py
@@ -1,6 +1,34 @@
 from ..ethercat.device import EtherCATDevice
+from ..ethercat.config import EtherCATConfig
 from ..cia_402.device import CiA402Device
 from ..errors.device import ErrorDevice
+
+
+class InovanceIS620NConfig(EtherCATConfig):
+    """Inovance IS620N servo drive config."""
+
+    # Device params non-volatile setting in 200C-0Eh:
+    # 0:  params not updated
+    # 1:  2000h series changed from serial or EtherCAT saved
+    # 2:  6000h series changed from EtherCAT (only) saved
+    # 3:  2000h and 6000h series changed from EtherCAT (only) saved
+
+    def get_device_params_nv(self):
+        return self.config.upload("200C-0Eh") == 3
+
+    def set_device_params_nv(self, nv=True, dry_run=False):
+        curr_setting = self.get_device_params_nv()
+        if curr_setting != nv:
+            self.logger.info(
+                f"{self} setting params {'non' if nv else ''}volatile mode"
+            )
+            self.config.download(
+                "200C-0Eh", (0, 3)[nv], force=True, dry_run=dry_run
+            )
+        else:
+            self.logger.info(
+                f"{self} params already {'non' if nv else ''}volatile mode"
+            )
 
 
 class InovanceIS620N(EtherCATDevice, CiA402Device, ErrorDevice):
@@ -12,10 +40,4 @@ class InovanceIS620N(EtherCATDevice, CiA402Device, ErrorDevice):
     xml_description_fname = "IS620N_v2.6.7.xml"
     device_error_package = "hw_device_mgr.devices.device_err"
     device_error_yaml = "inovance_is620n.yaml"
-
-    def set_params_volatile(self, nv=False):
-        # 0:  params not updated
-        # 1:  2000h series changed from serial or EtherCAT saved
-        # 2:  6000h series changed from EtherCAT (only) saved
-        # 3:  2000h and 6000h series changed from EtherCAT (only) saved
-        self.config.download("200C-0Eh", (0, 3)[nv])
+    config_class = InovanceIS620NConfig
--- hw_device_mgr/devices/inovance_sv660.py
+++ hw_device_mgr/devices/inovance_sv660.py
@@ -1,6 +1,34 @@
 from ..ethercat.device import EtherCATDevice
+from ..ethercat.config import EtherCATConfig
 from ..cia_402.device import CiA402Device
 from ..errors.device import ErrorDevice
+
+
+class InovanceSV660Config(EtherCATConfig):
+    """Inovance SV660 servo drive config."""
+
+    # Device params non-volatile setting in 200E-02h:
+    # 0:  params not updated
+    # 1:  2000h series changed from serial or EtherCAT saved
+    # 2:  6000h series changed from EtherCAT (only) saved
+    # 3:  2000h and 6000h series changed from EtherCAT (only) saved
+
+    def get_device_params_nv(self):
+        return self.config.upload("200E-02h") == 3
+
+    def set_device_params_nv(self, nv=True, dry_run=False):
+        curr_setting = self.get_device_params_nv()
+        if curr_setting != nv:
+            self.logger.info(
+                f"{self} setting params {'non' if nv else ''}volatile mode"
+            )
+            self.config.download(
+                "200E-02h", (0, 3)[nv], force=True, dry_run=dry_run
+            )
+        else:
+            self.logger.info(
+                f"{self} params already {'non' if nv else ''}volatile mode"
+            )
 
 
 class InovanceSV660(EtherCATDevice, CiA402Device, ErrorDevice):
@@ -12,10 +40,4 @@ class InovanceSV660(EtherCATDevice, CiA402Device, ErrorDevice):
     xml_description_fname = "SV660_EOE_1Axis_V9.12.xml"
     device_error_package = "hw_device_mgr.devices.device_err"
     device_error_yaml = "inovance_sv660n.yaml"
-
-    def set_params_volatile(self, nv=False):
-        # 0:  params not updated
-        # 1:  2000h series changed from serial or EtherCAT saved
-        # 2:  6000h series changed from EtherCAT (only) saved
-        # 3:  2000h and 6000h series changed from EtherCAT (only) saved
-        self.config.download("200E-02h", (0, 3)[nv])
+    config_class = InovanceSV660Config
