3d54c69a9c34aab6ce23a9e2133bed1247a10e6b
John Morris <john@zultron.com>
2022-07-10 14:17:48 -0500

    mgr:  Rework to expose per-device command_in attributes
    In order to support homing devices individually, the manager must
    expose separate `home_request` command for each drive.  This is a
    substantial change, since some device command still comes from the
    manager, and now some comes from the outside.  The manager's
    `command_in` interface is no longer static, and per-device keys must
    be added after drives are enumerated during the bus scan.
    
    The incoming command is also changed so that command doesn't anymore
    come in through the `feedback_in` interface and pass through
    `feedback_out`, and pass into `command_in` after manager munging.  As
    a result, the feedback interfaces also change quite a bit.
    
    Because of the dynamic changes to initialization, the many init
    functions are combined so that using code can simply create the
    manager object and call the single `init()` method.  No more separate
    `init()`, `init_sim`, `init_devices()`.
    
    Many (though not yet enough) CiA 301 & 402 functions were moved out of
    the manager and into the devices so the manager can be device agnostic
    and manage more types of devices.  This is true of homing (completely
    removed from the state machine), drive control mode, and status &
    control word flags.  This simplifies the top-level manager logic.  It
    also requires big changes to the tests, whose routines are adapted for
    this new scheme, and test cases are extensively updated.

--- hw_device_mgr/mgr/tests/test_mgr.py
+++ hw_device_mgr/mgr/tests/test_mgr.py
@@ -1,8 +1,8 @@
 from .base_test_class import BaseMgrTestClass
 from ...tests.test_device import TestDevice as _TestDevice
-from ...cia_402.device import CiA402Device
 import re
 import pytest
+from functools import lru_cache, cached_property
 
 
 class TestHWDeviceMgr(BaseMgrTestClass, _TestDevice):
@@ -17,12 +17,36 @@ class TestHWDeviceMgr(BaseMgrTestClass, _TestDevice):
     @pytest.fixture
     def obj(self, device_cls, mgr_config, device_config, all_device_data):
         self.obj = device_cls()
-        self.obj.init(mgr_config=mgr_config)
-        self.obj.init_sim(sim_device_data=all_device_data.values())
-        self.obj.init_devices(device_config=device_config)
+        self.obj.init(
+            mgr_config=mgr_config,
+            device_config=device_config,
+            sim_device_data=all_device_data.values(),
+        )
         yield self.obj
 
-    drive_key_re = re.compile(r"^drive_([x0-9])_(.*)$")
+    test_case_key_re = re.compile(r"^d\.([x0-9])\.(.*)$")
+    drive_interface_key_re = re.compile(r"^d([0-9])\.([0-9]+)\.(.*)$")
+
+    @cached_property
+    def drive_addr_to_index_map(self):
+        return {d.address: i for i, d in enumerate(self.obj.devices)}
+
+    @lru_cache
+    def obj_interface_to_test_case_key(self, interface_key):
+        # Translate test object interface key `d0.16.control_mode` to test case
+        # key `d.6.control_mode`
+        m = self.drive_interface_key_re.match(interface_key)
+        if m is None:
+            return interface_key  # Doesn't match fmt. `d.6.control_mode`
+        address = tuple(map(int, m.groups()[:-1]))  # (0, 16)
+        index = self.drive_addr_to_index_map[address]  # 6
+        key = m.groups()[-1]  # "control_mode"
+        return f"d.{index}.{key}"
+
+    def obj_interface_to_test_case(self, data):
+        return {
+            self.obj_interface_to_test_case_key(k): v for k, v in data.items()
+        }
 
     def test_init(self, obj, all_device_data):
         super().test_init(obj)
@@ -42,73 +66,113 @@ class TestHWDeviceMgr(BaseMgrTestClass, _TestDevice):
             if "error_code" in d.feedback_out.get():
                 # Account for some devices that inherit from ErrorDevice
                 self.test_data["feedback_in"].setdefault(
-                    f"drive_{i}_error_code", 0x00000000
+                    f"d.{i}.error_code", 0x00000000
                 )
                 self.test_data["feedback_out"].setdefault(
-                    f"drive_{i}_error_code", 0x00000000
+                    f"d.{i}.error_code", 0x00000000
                 )
                 self.test_data["feedback_out"].setdefault(
-                    f"drive_{i}_description", "No error"
+                    f"d.{i}.description", "No error"
                 )
                 self.test_data["feedback_out"].setdefault(
-                    f"drive_{i}_advice", "No error"
+                    f"d.{i}.advice", "No error"
                 )
 
         uint16 = self.device_class.data_type_class.uint16
-        control_mode_int = CiA402Device.control_mode_int
         for data in (self.test_data, self.ovr_data):
             for intf, intf_data in data.items():
                 for key in intf_data.keys():
                     # Test drive keys
-                    match = self.drive_key_re.match(key)
+                    match = self.test_case_key_re.match(key)
                     if not match:
                         continue  # Only testing drive keys
-                    dkey = match.group(2)
-                    if dkey == "control_mode" and intf == "command_out":
-                        # Translate control_mode, e.g. MODE_CSP -> 8
-                        intf_data[key] = control_mode_int(intf_data[key])
-                    elif dkey == "control_mode_fb" and intf in (
-                        "sim_feedback",
-                        "feedback_in",
-                    ):
-                        # Translate control_mode, e.g. MODE_CSP -> 8
-                        intf_data[key] = control_mode_int(intf_data[key])
-                    elif dkey in ("status_word", "control_word"):
+                    if match.group(2) in ("status_word", "control_word"):
                         # Format status_word, control_word for
                         # readability, e.g. 0x000F
                         intf_data[key] = uint16(intf_data[key])
 
     def munge_test_case_data(self, test_case, dst, suffix=""):
-        # Expand drive_x_foo -> drive_1_foo, etc.; don't clobber
+        # Expand d.x.foo -> d.1.foo, etc.; don't clobber
         # specific case
         for intf in self.device_class.interface_names:
             values = test_case.get(intf + suffix, dict())
             intf_dst = dst.setdefault(intf, dict())
             for key, val in values.items():
-                match = self.drive_key_re.match(key)
+                match = self.test_case_key_re.match(key)
                 if not match:
                     intf_dst[key] = val
-                    continue
+                    continue  # Regular attribute, not d.x.foo
                 d_ix, d_key = match.groups()
                 if d_ix == "x":
                     for i, dev in enumerate(self.obj.devices):
-                        dev_key = f"drive_{i}_{d_key}"
+                        dev_key = f"d.{i}.{d_key}"
                         if dev_key not in values:
                             intf_dst[dev_key] = val
                 else:
-                    intf_dst[key] = val
+                    d_ix = int(d_ix)
+                    dev_key = f"d.{d_ix}.{d_key}"
+                    intf_dst[dev_key] = val
+
+    def munge_interface_data(self, interface):
+        # Translate test case key `d.6.control_mode` to object interface key
+        # `d0.16.control_mode`
+        data_raw = super().munge_interface_data(interface)
+        data = dict()
+        for key, val in data_raw.items():
+            m = self.test_case_key_re.match(key)
+            if m is None:
+                # Doesn't match fmt. `d.6.control_mode`
+                data[key] = val
+            else:
+                index, key = m.groups()  # ("6", "control_mode")
+                dev = self.obj.devices[int(index)]
+                pfx = self.obj.dev_prefix(dev, suffix=dev.slug_separator)
+                dev_key = f"{pfx}{key}"  # "d0.16.control_mode"
+                data[dev_key] = val
+
+        return data
+
+    def override_interface_param(self, interface, ovr_data):
+        for key, val in ovr_data.items():
+            match = self.test_case_key_re.match(key)
+            if match:
+                index, key = match.groups()
+                intf = self.obj.devices[int(index)].interface(interface)
+                intf.update(**{key: val})
+            else:
+                super().override_interface_param(interface, {key: val})
+
+    def check_interface_values(self, interface, indent=4):
+        if interface in {"feedback_out", "command_in"}:
+            # Higher level interfaces to application
+            return self.check_interface_values_higher(interface, indent=indent)
+
+        else:
+            # Lower level interfaces close to hardware interface
+            return self.check_interface_values_lower(interface, indent=indent)
+
+    def check_interface_values_higher(self, interface, indent=4):
+        # Check feedback_in, command_out, sim interfaces
+        expected = self.test_data[interface]
+        actual_raw = self.obj.interface(interface).get()
+        actual = self.obj_interface_to_test_case(actual_raw)
+        if super().check_data_values(interface, expected, actual, indent):
+            return True
+        else:
+            print(f"FAILURE at {self.test_desc}")
+            return False
 
     def split_drive_data(self, data):
         # The manager is a device, and each of obj.devices is a
         # device.  The test cases are crammed together with device
-        # keys prefixed with `drive_x_`, meaning it applies to all
-        # drives, or prefixed with `drive_0_`, meaning it applies to
+        # keys prefixed with `d.x.`, meaning it applies to all
+        # drives, or prefixed with `d.0.`, meaning it applies to
         # the first drive, or prefixed with nothing, meaning it
         # applies to the manager.
         mgr_data = data.copy()
         device_data = list([dict() for d in self.obj.devices])
-        for drive_key in list(mgr_data.keys()):
-            match = self.drive_key_re.match(drive_key)
+        for drive_key, val in list(mgr_data.items()):
+            match = self.test_case_key_re.match(drive_key)
             if not match:
                 continue  # Not a drive_key
             val = mgr_data.pop(drive_key)
@@ -116,16 +180,9 @@ class TestHWDeviceMgr(BaseMgrTestClass, _TestDevice):
             device_data[int(index)][key] = val
         return mgr_data, device_data
 
-    def override_interface_param(self, interface, key, val):
-        match = self.drive_key_re.match(key)
-        if match:
-            index, key = match.groups()
-            intf = self.obj.devices[int(index)].interface(interface)
-            intf.update(**{key: val})
-        else:
-            super().override_interface_param(interface, key, val)
+    def check_interface_values_lower(self, interface, indent=4):
+        # Check feedback_in, command_out, sim interfaces
 
-    def check_interface_values(self, interface, indent=4):
         # Prepare expected data
         expected = self.test_data[interface]
         mgr_expected, device_expected = self.split_drive_data(expected)
@@ -147,21 +204,9 @@ class TestHWDeviceMgr(BaseMgrTestClass, _TestDevice):
             actual = device.interface(interface).get()
             # self.print_dict(actual, f"drive_{i}", indent=4)
             passing &= self.check_data_values(
-                "", device_expected[i], actual, indent, f"drive_{i}_"
+                "", device_expected[i], actual, indent=indent, prefix=f"d.{i}."
             )
 
         if not passing:
             print(f"FAILURE at {self.test_desc}")
         return passing
-
-    def set_command_and_check(self):
-        print("\n*** Running object set_command()")
-        # Mgr command_in is internally generated; no args to set_command()
-        # (Drive command is set by mgr; no need to call that separately)
-        self.obj.set_command()
-        success = self.check_interface_values("command_in")
-        success = self.check_interface_values("command_out") and success
-        assert success
-        print("\n*** Overriding command_out")
-        self.override_data("command_out")
-        # self.print_dict(self.test_data, "Test data (after override)")
