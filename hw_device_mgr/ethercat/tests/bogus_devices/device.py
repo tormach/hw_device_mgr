84d5597e3154283e48c0fe654aa6993cb587eec7
John Morris <john@zultron.com>
2022-07-10 14:11:52 -0500

    ethercat:  Updates for recent Interfaces changes

--- hw_device_mgr/ethercat/tests/bogus_devices/device.py
+++ hw_device_mgr/ethercat/tests/bogus_devices/device.py
@@ -1,8 +1,10 @@
 from ..relocatable_esi_device import RelocatableESIDevice
 from ....cia_402.device import CiA402SimDevice
+from ....tests.interface import DebugInterface
 
 
 class BogusEtherCATDevice(RelocatableESIDevice):
+    interface_class = DebugInterface
     category = "bogus_ethercat_devices"
     vendor_id = 0xB090C0
     xml_description_package = "hw_device_mgr.devices.device_xml"
