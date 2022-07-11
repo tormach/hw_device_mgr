d9e17cf60734dd1cd440031ef32ed8dece47576f
John Morris <john@zultron.com>
2022-07-11 17:18:16 -0500

    mgr_ros_hal:  Updates for recent changes in init()
    Update entrypoint with simplified `init() call.

--- hw_device_mgr/mgr_ros_hal/__main__.py
+++ hw_device_mgr/mgr_ros_hal/__main__.py
@@ -4,12 +4,8 @@ from hw_device_mgr.mgr_ros_hal.mgr import ROSHALHWDeviceMgr
 
 def main(args=None):
     argv = list(sys.argv[1:])  # Knock off executable name
-    sim = "--sim" in argv
     mgr = ROSHALHWDeviceMgr()
-    mgr.init(argv)
-    if sim:
-        mgr.init_sim_from_rosparams()
-    mgr.init_devices()
+    mgr.init(argv=argv)
     mgr.run()
 
 
