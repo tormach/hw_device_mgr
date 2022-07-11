75831c2739911f20e3af018e441c432b99127142
John Morris <john@zultron.com>
2022-07-11 17:18:16 -0500

    mgr_ros_hal:  Updates for recent Interfaces changes
    Use `DebugInterface` in tests

--- hw_device_mgr/mgr_ros_hal/tests/bogus_devices/mgr.py
+++ hw_device_mgr/mgr_ros_hal/tests/bogus_devices/mgr.py
@@ -1,8 +1,10 @@
 from ...mgr import ROSHALSimHWDeviceMgr
 from ....mgr_hal.tests.bogus_devices.mgr import HALHWDeviceMgrTestCategory
+from ....tests.interface import DebugInterface
 
 
 class ROSHWDeviceMgrTestCategory(ROSHALSimHWDeviceMgr):
+    interface_class = DebugInterface
     category = "test_ros_hal_hw_device_mgr"
     data_type_class = HALHWDeviceMgrTestCategory.data_type_class
     device_base_class = HALHWDeviceMgrTestCategory.device_base_class
