4c21944fae884d172b8f59ef311b4551e89b488c
John Morris <john@zultron.com>
2022-07-11 17:11:53 -0500

    mgr_hal:  Fix HAL pin init and add fb_out and cmd_in HAL pins
    The `HALHWDeviceMgr` class inherits from `HALCompDevice` before
    `HWDeviceMgr` so that the former's `init()` function initializes the
    HAL comp before the latter's `init()` function runs, and afterwards,
    marks the HAL comp `ready()`.
    
    Set empty `slug_separator` to avoid prefixing pins with e.g. `.reset`.

--- hw_device_mgr/mgr_hal/mgr.py
+++ hw_device_mgr/mgr_hal/mgr.py
@@ -2,12 +2,24 @@ from ..mgr.mgr import HWDeviceMgr, SimHWDeviceMgr
 from ..hal.device import HALCompDevice, HALPinDevice, HALPinSimDevice
 
 
-class HALHWDeviceMgr(HWDeviceMgr, HALCompDevice):
+class HALHWDeviceMgr(HALCompDevice, HWDeviceMgr):
     """Hardware device manager with HAL pins."""
 
+    # Inherit from `HALCompDevice` first to ensure `HALCompDevice.init()` sets
+    # `self.comp` first, `HWDeviceMgr.init()` calls `HALPinDevice.init()` then
+    # `HWDeviceMgr.init_devices()`, then returns to `HALCompDevice.init()` to
+    # call `comp.ready()`.
+
     hal_comp_name = "hw_device_mgr"
-    data_type_class = HALCompDevice.data_type_class
     device_base_class = HALPinDevice
+    slug_separator = ""
+
+    pin_interfaces = dict(
+        feedback_in=(HALPinDevice.HAL_IN, ""),
+        feedback_out=(HALPinDevice.HAL_OUT, ""),
+        command_in=(HALPinDevice.HAL_IN, ""),
+        command_out=(HALPinDevice.HAL_OUT, ""),
+    )
 
     def init_devices(self, /, device_init_kwargs=dict(), **kwargs):
         device_init_kwargs["comp"] = self.comp
