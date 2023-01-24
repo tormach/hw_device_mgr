32b821382f01534a5b2fc461bbe68fa79637756b
John Morris <john@zultron.com>
2023-01-23 21:01:29 -0600

    errors:  Remove bogus test

--- hw_device_mgr/errors/tests/test_device.py
+++ hw_device_mgr/errors/tests/test_device.py
@@ -24,13 +24,3 @@ class TestErrorDevice(ErrorBaseTestClass, _TestDevice):
             assert isinstance(data["description"], str)
             assert isinstance(data.get("advice", ""), str)
             print(f"   code {err_code}:  {data['description']}")
-
-    def test_set_feedback(self, obj):
-        errs = obj.error_descriptions()
-        assert len(errs) > 0
-        for err_code, data in errs.items():
-            obj.set_feedback(error_code=err_code)
-            fb = obj.feedback()
-            for key, val in data.items():
-                assert fb[key] == val
-        assert 0
