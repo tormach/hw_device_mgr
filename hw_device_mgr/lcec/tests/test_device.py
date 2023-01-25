83d1aca4d5bd4036e3c4b6e07138a0f124e1c37f
John Morris <john@zultron.com>
2023-01-24 18:10:41 -0600

    lcec:  Fix MRO test

--- hw_device_mgr/lcec/tests/test_device.py
+++ hw_device_mgr/lcec/tests/test_device.py
@@ -20,9 +20,7 @@ class TestLCECDevice(BaseLCECTestClass, _TestEtherCATDevice, _TestHALDevice):
     ]
 
     @pytest.fixture
-    def obj(self, sim_device_data, mock_halcomp):
-        self.obj = self.device_model_cls(
-            address=sim_device_data["address"]
-        )
+    def obj(self, sim_device_data, mock_halcomp, device_cls):
+        self.obj = self.device_model_cls(address=sim_device_data["address"])
         self.obj.init(comp=mock_halcomp)
         yield self.obj
