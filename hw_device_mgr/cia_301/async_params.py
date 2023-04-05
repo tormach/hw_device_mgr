7b3669212aa12a80c49c46a1d8e4aa96329f85fc
John Morris <john@zultron.com>
2023-04-04 20:12:16 -0500

    cia_301:  `async_params` module initial commit

---
+++ hw_device_mgr/cia_301/async_params.py
@@ -0,0 +1,15 @@
+from ..async_task_queue import AsyncTaskQueue
+
+class AsyncParamsQueue(AsyncTaskQueue):
+    """Process device configuration commands in an asynchronous queue."""
+
+    def process_cmd(self, cmd_raw):
+        """Execute one config command in worker."""
+        config, cmd, args, kwargs = cmd_raw
+        return getattr(config, cmd)(*args, **kwargs)
+
+    def download(self, config, values, **cmd_kwargs):
+        """Enqueue one param config 'download' command."""
+        for sdo, val in values.items():
+            cmd = (config, 'download', (sdo, val), cmd_kwargs)
+            super().enqueue(cmd)
