de603bc64e9c028eb0b101d441e1eafde1bfaa6d
John Morris <john@zultron.com>
2023-05-02 10:54:55 -0500

    devices:  Add sim junction box and IO module classes
    Junction box ESI is unusual because it doesn't contain mailboxes or
    object dictionary definitions.

--- hw_device_mgr/devices/bogus.py
+++ hw_device_mgr/devices/bogus.py
@@ -23,3 +23,28 @@ class BogusV2Servo(BogusDevice):
     """Bogus Device Co V2 servo drive."""
 
     product_code = 0xB0905031
+
+
+class BogusOtherDevice(EtherCATSimDevice):
+    """Base class for Bogus Device Co devices other than motors."""
+
+    category = "bogus_other"
+    vendor_id = 0xB090C0
+    xml_description_package = "hw_device_mgr.devices.device_xml"
+    device_error_package = "hw_device_mgr.devices.device_err"
+    device_error_yaml = "unpopulated.yaml"
+
+
+class BogusJBox(BogusOtherDevice):
+    """Bogus Device Co Junction Box."""
+
+    product_code = 0xB0908030
+    xml_description_fname = "BogusJunction.xml"
+
+
+class BogusIO(BogusOtherDevice):
+    """Bogus Device Co V1 IO Module."""
+
+    product_code = 0xB0901030
+    xml_description_fname = "BogusIO.xml"
+    device_error_yaml = "bogus_v1_error_io.yaml"
